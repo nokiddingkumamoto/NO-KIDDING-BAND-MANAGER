7faf4d4cf4201572e362af85432c3c8344d4074c
nokiddingkumamoto <kxcxsxp@gmail.com>
2026-07-16 16:28:57 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="nkbm-v200";
-const ASSETS=["./","index.html","styles.css?v=200","app.js?v=200","manifest.webmanifest","assets/logo.png","assets/character.png","assets/icon.svg"];
+const CACHE="nkbm-v210";
+const ASSETS=["./","index.html","styles.css?v=210","app.js?v=210","manifest.webmanifest","assets/logo.png","assets/character.png","assets/icon.svg"];
 self.addEventListener("install",event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS).catch(()=>{})))});
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{
